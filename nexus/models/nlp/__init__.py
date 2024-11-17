61ec7426becd3770453412e41fcd48d83a2dc23b
Kevin Yu <kevin@2046inc.xyz>
2024-11-17 16:11:33 -0500

    Add basic hallucination redudction classes

--- nexus/models/nlp/__init__.py
+++ nexus/models/nlp/__init__.py
@@ -1,5 +1,6 @@
 from .edge_llm import EdgeLLM
 from .chain_of_thoughts import ChainOfThoughtModule
-from .rag import RAGModule
+from .rag import RAGModule, EfficientRetriever, DocumentEncoder
+from .hallucination_reducer import HallucinationReducer
 
-__all__ = ['EdgeLLM', 'ChainOfThoughtModule', 'RAGModule'] 
+__all__ = ['EdgeLLM', 'ChainOfThoughtModule', 'RAGModule', 'HallucinationReducer', 'EfficientRetriever', 'DocumentEncoder'] 
