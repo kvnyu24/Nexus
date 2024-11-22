7a9acfd36b6913044c4a450e709572d2f18dc879
Kevin Yu <kevin@2046inc.xyz>
2024-11-22 00:46:59 -0600

    Add t5 and longformer models

--- nexus/models/nlp/__init__.py
+++ nexus/models/nlp/__init__.py
@@ -2,5 +2,17 @@ from .edge_llm import EdgeLLM
 from .chain_of_thoughts import ChainOfThoughtModule
 from .rag import CrossAttentionFusion, EnhancedRAGModule, EfficientRetriever, DocumentEncoder
 from .hallucination_reducer import HallucinationReducer
+from .t5 import EnhancedT5
+from .longformer import Longformer
 
-__all__ = ['EdgeLLM', 'ChainOfThoughtModule', 'CrossAttentionFusion', 'EnhancedRAGModule', 'HallucinationReducer', 'EfficientRetriever', 'DocumentEncoder'] 
+__all__ = [
+    'EdgeLLM',
+    'ChainOfThoughtModule',
+    'CrossAttentionFusion',
+    'EnhancedRAGModule',
+    'HallucinationReducer',
+    'EfficientRetriever',
+    'DocumentEncoder',
+    'EnhancedT5',
+    'Longformer'
+] 
